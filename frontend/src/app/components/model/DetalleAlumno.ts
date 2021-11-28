00decb1bbba341dde37cc1300e10ab370370d5b6
4lequinn <jorgealequinn@gmail.com>
2021-11-28 00:07:50 -0300

    Modificar asistencia OK

---
+++ frontend/src/app/components/model/DetalleAlumno.ts
@@ -0,0 +1,9 @@
+export interface DetalleI{
+    alumnoID:number;
+    nombre:string;
+    apellido:string;
+    presente:boolean;
+    carrera:string;
+    ramo:string;
+    usuario: string;
+}
--- frontend/src/app/pages/alumnos/detalle-alumno/detalle-alumno.page.ts
+++ frontend/src/app/pages/alumnos/detalle-alumno/detalle-alumno.page.ts
@@ -3,6 +3,9 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { alumnos } from '../alumnos.model'
 import { AlertController } from '@ionic/angular';
 import { AlumnosService } from 'src/app/services/alumnos.service';
+import { ApiServiceService } from 'src/app/services/api-service.service';
+import { DetalleI } from 'src/app/components/model/DetalleAlumno';
+import { AsistenciaI } from 'src/app/components/model/Asistencia';
 
 @Component({
   selector: 'app-detalle-alumno',
@@ -11,17 +14,100 @@ import { AlumnosService } from 'src/app/services/alumnos.service';
 })
 export class DetalleAlumnoPage implements OnInit {
 
+  // Modelo de alumno
+  alumno: DetalleI;
 
-  alumnos: any = [];
+  // ID 
+  id : any;
+
+  //Estados de asistencia PRESENTE - AUSENTE
+  estados : boolean[] = [true , false];
+
+  // Guardamos el valor elegido
+  valorSeleccionado: string;
+
+  // Modelo asistencia
+  asistencia : AsistenciaI;
 
 
-  constructor(private alumnosService: AlumnosService, private alertController: AlertController, private router: Router) { }
+  constructor
+    (
+      private alumnosService: AlumnosService,
+      private apiService: ApiServiceService,
+      private alertController: AlertController,
+      private router: Router,
+      private activatedRoute : ActivatedRoute
+  ) { }
 
-   async deleteAlumno(id){
+
+  ngOnInit() {
+    // Recibo el ID por URL
+    this.id = this.activatedRoute.snapshot.paramMap.get("id");
+    this.detalleAlumno(this.id);
+  }
+
+  valorCambiado(){
+    // Método que captura el valor seleccionado actual del radio button
+    console.log(this.valorSeleccionado);
+  }
+
+
+  detalleAlumno(id) {
+    this.apiService.buscarAlumno(id).subscribe(
+      (data) => {
+        console.log(data);
+        this.alumno = {
+          alumnoID : data.id,
+          nombre : data.nombre,
+          apellido : data.apellido,
+          carrera : data.carrera.descripcion ,
+          ramo : data.ramos[0].descripcion,
+          usuario : data.usuario.nombre_usuario,
+          presente : data.presente
+        }
+        
+        if(this.alumno.presente){
+          this.valorSeleccionado = "presente";
+        }else{
+          this.valorSeleccionado = "ausente";
+        }
+        console.log('Valor seleccionado ' + this.valorSeleccionado);
+      },
+      (e) => {
+        console.log('Error al cargar');
+      }
+    )
+
+  }
+
+
+  actualizar(id){
+    var estado = false;
+    // Capturar el estado de asistencia seleccionado 
+    if(this.valorSeleccionado == "presente"){
+      estado = true;
+    }
+
+    this.asistencia = {
+      presente : estado
+    }
+
+    this.apiService.modificarAlumno(id, this.asistencia).subscribe(
+      (data) => {
+        console.log('Se actualizó la asistencia con éxito!');
+        this.router.navigate(['/alumnos']);
+      },
+      (e) => {
+        console.log('Error no se puede actualizar la asistencia');
+      }
+    )
+  }
+
+  async eliminarAlumno(id) {
     //console.log(id);
-    const alerta = await this.alertController.create( {
-      header: 'Eliminando Alumno',
-      message: 'Esta Seguro que Quiere Eliminar al Alumno??',
+    const alerta = await this.alertController.create({
+      header: 'Eliminar Alumno',
+      message: '¿Está Seguro que quiere Eliminar al Alumno?',
       buttons: [
         {
           text: 'Cancelar',
@@ -47,31 +133,8 @@ export class DetalleAlumnoPage implements OnInit {
 
   }
 
-  buscarIndividual(id: String){
-    this.alumnosService.detalleAlumno(id).subscribe( 
-      (res) => {
-        console.log(res);
-        this.alumnos = res;
-      },
-      (err) => {
-        console.log(err);
-      }
-    );
-  }
 
 
-  ngOnInit() {
-    this.alumnosService.getAll().subscribe( 
-      (res) => {
-        console.log(res);
-        this.alumnos = res;
-      },
-      (err) => {
-        console.log(err);
-      }
-    );
 
-
-  }
 
 }
